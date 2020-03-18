5d0c330fcd2fa538e396f5187973d3f6f988230c
Stephen Liu <ssl76@case.edu>
2020-03-17 23:08:09 -0400

    Implement changes suggested by Prof. Meckes
    - fix a number of typos in the slides.
    - include the magnitude of a line segment.
    - add in a number of figures for Schröder paths.
    - add values predicted by CMC on graph.

--- src/secondorder.m
+++ src/secondorder.m
@@ -1,10 +1,11 @@
-ds = [1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41];
+ds = [3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41];
 seconds = zeros(length(ds),1);
 secondsmu = zeros(length(ds),1);
 secondsimp = zeros(length(ds),1);
 firsts = zeros(length(ds),1);
 quos = zeros(length(ds),1);
 quos2 = zeros(length(ds),1);
+preds = zeros(length(ds),1);
 
 for i = 1:length(ds)
     d = ds(i);
@@ -17,12 +18,18 @@ for i = 1:length(ds)
     N2p = N2p0(m);
     D2p = D2p0(m);
 
+    % Calculate second derivative term
     seconds(i) = 2*((N2p - factorial(d)*D2p)/(factorial(d)*D)) - (V1(m)*Dp/D);
     secondsmu(i) = 2*(mu(m)/N) - (V1(m)*Dp/D);
     secondsimp(i) = (6*sig2sumovern(m)) - (V1(m)*DpoverD(m));
     quos(i) = (secondsmu(i)/seconds(i));
     quos2(i) = (secondsimp(i)/secondsmu(i));
+    
+    % First derivative term
     firsts(i) = 0.5*V1(m);
+    
+    % Calculate prediction by convex magnitude conjecture
+    preds(i) = (nchoosek(ds(i),2)/(ds(i)));
 end
 
 figure(1);
@@ -35,6 +42,9 @@ figure(1);
 % legend('Firsts', 'Seconds', 'Secondsmu', 'Secondsimp', 'Location', 'NorthWest');
 
 plot(ds, secondsimp, 'bo');
+hold on;
+plot(ds, preds, 'go');
+legend('Second order term', 'Prediction by CMC', 'Location', 'NorthWest');
 xlabel('Dimension d');
 ylabel('Second order term');
 title('Second order term vs. Dimension');
